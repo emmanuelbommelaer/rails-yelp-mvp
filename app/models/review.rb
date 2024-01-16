b39991d48cbdff7243b5dc0b73345bdff00acbb5
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-01-16 20:14:05 +0100

    drop everything once in a while

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,3 @@
 class Review < ApplicationRecord
-  belongs_to :restaurant, class_name: "restaurant", foreign_key: "restaurant_id"
-
-  validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: (0..5).to_a }
+  belongs_to :restaurant
 end
